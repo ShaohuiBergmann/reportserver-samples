210b0f2c01c39a3621897aa5a85bdc3978333f86
ShaohuiBergmann <milashaohuibergmann@gmail.com>
2022-12-14 16:10:47 +0100

    got the JSON to frontend

--- src/net/datenwerke/rs/samples/script.js
+++ src/net/datenwerke/rs/samples/script.js
@@ -1,3 +1,31 @@
 (function () {
- $("#btn").click()
+	const getContent = () => {
+		$.ajax({
+			url: "http://127.0.0.1:8080/reportserver/scriptAccess?path=/bin/exampleLog.groovy",
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+				"Access-Control-Allow-Origin": "*",
+			},
+			success: function (response) {
+				console.log("Response from network request");
+				console.log("response: ", response);
+
+				// var htmlResponse = "";
+
+				// for (let i = 0; i < response.length; i++) {
+				// 	console.log("response[i].title: ", response[i].title);
+				// 	htmlResponse += "<p>" + response[i].title + "</p>";
+				// }
+
+				// $(".results").html(htmlResponse);
+			},
+		});
+
+		// $.get("/data.json", function (data) {
+		// 	console.log("data: ", data);
+		// });
+	};
+
+	getContent();
 })();
