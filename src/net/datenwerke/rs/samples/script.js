68cb3bc134a5e888d89878ec664a4dc958e8e526
ShaohuiBergmann <milashaohuibergmann@gmail.com>
2023-01-18 16:48:41 +0100

    solved the options problem, only one file was able to get logged in the textarea

--- src/net/datenwerke/rs/samples/script.js
+++ src/net/datenwerke/rs/samples/script.js
@@ -18,7 +18,25 @@
 			},
 		});
 	};
-	// function that convert date format e.g. 2023-01-12 to the format of 12-Dec-2022
+	//////////function that decode and parse the result from getContent and filterout null and empty files
+	const getFilteredFileArr = (arr) => {
+		const decoded = JSON.parse(atob(arr));
+		const fileArr = decoded["data"];
+		return filterFileArr(fileArr);
+	};
+	///////////create new array of objects that has the filepath as key and content as value
+	const createNewArrOfVar = (arr) => {
+		const newArrOfVal = [];
+		arr.forEach((el) => {
+			const arrOfVal = Object.values(el);
+			const newObj = createObj(arrOfVal);
+			newArrOfVal.push(newObj);
+			// console.log(newArrOfVal);
+		});
+
+		return newArrOfVal;
+	};
+	///////////function that convert date format e.g. 2023-01-12 to the format of 12-Dec-2022
 	function convertDate(inputValue) {
 		const date = new Date(inputValue);
 		const options = { day: "numeric", month: "short", year: "numeric" };
@@ -30,7 +48,7 @@
 		console.log("converting to", convertedDate);
 		return convertedDate;
 	}
-	// function that declare an object {filepath: content}
+	///////////function that declare an object {filepath: content}
 
 	const createObj = (arr) => {
 		const fileObj = {};
@@ -40,58 +58,70 @@
 		return fileObj;
 	};
 
-	// filtered out the files that don't contain any content
+	///////////filtered out the files that don't contain any content
 	const filterFileArr = (arr) => {
 		const filteredFileArr = arr.filter((el) => {
 			if (el) {
 				return el.content !== "";
 			}
 		});
+
 		return filteredFileArr;
 	};
 
-	// function to add option elements to Dom
+	///////////functions to manipulate option elements to Dom
 
-	const addOptions = (text) => {
-		const opt = document.createElement("option");
+	const updateNodes = (selector, element, text) => {
+		const ele = document.createElement(element);
 		const node = document.createTextNode(text);
-		opt.appendChild(node);
-		document.getElementById("opts").appendChild(opt);
+		ele.appendChild(node);
+		document.getElementById(selector).appendChild(ele);
 	};
 
-	///////////////////////////////
-	$("#btn").click(() => {
-		console.log("click happened");
-		const dateInputVal = $("#dateVal").val();
-		console.log(dateInputVal);
-		const convertedResult = convertDate(dateInputVal);
-		try {
-			getContent()
-				.then((result) => {
-					const decoded = JSON.parse(atob(result));
-					const fileArr = decoded["data"];
-					const filteredArr = filterFileArr(fileArr);
-					const newArrOfVal = [];
-					filteredArr.forEach((el) => {
-						const arrOfVal = Object.values(el);
-						const newObj = createObj(arrOfVal);
-						newArrOfVal.push(newObj);
-						// console.log(newArrOfVal);
-						newArrOfVal.forEach((el) => {
-							Object.keys(el).forEach((prop) => {
-								// console.log(el[prop]);
-								const dateInputVal = $("#dateVal").val();
-								if (prop.includes(dateInputVal)) {
-									console.log(prop);
-									addOptions(prop);
-								}
-							});
-						});
-					});
-				})
-				.catch((e) => console.log("catch e", e));
-		} catch (e) {
-			console.log(e);
+	const removeNodes = (selector) => {
+		const list = document.getElementById(selector);
+		while (list.hasChildNodes()) {
+			list.removeChild(list.firstChild);
 		}
+	};
+	const handleEmptyResult = (selector, element) => {
+		removeNodes(selector);
+		const ele = document.createElement(element);
+		const node = document.createTextNode("Sorry, No result found");
+		ele.appendChild(node);
+		document.getElementById(selector).appendChild(ele);
+	};
+
+	//////////////////event listener
+	$("#dateVal").change((e) => {
+		getContent().then((result) => {
+			const filteredFileArr = getFilteredFileArr(result);
+			const newArrOfVal = createNewArrOfVar(filteredFileArr);
+			console.log("arr", newArrOfVal);
+			removeNodes("opts");
+			removeNodes("logContent");
+			const arrOfKeys = [];
+			newArrOfVal.forEach((el) => {
+				const dateInputVal = e.target.value;
+				if (Object.keys(el)[0].includes(dateInputVal)) {
+					arrOfKeys.push(Object.keys(el)[0]);
+					console.log("arrKy", arrOfKeys);
+				}
+
+				$("#opts").change((e) => {
+					removeNodes("logContent");
+					const valOfOpts = e.target.value;
+					if (el[valOfOpts]) {
+						updateNodes("logContent", "p", el[valOfOpts]);
+					} else {
+						removeNodes("logContent");
+						handleEmptyResult("logContent", "p");
+					}
+				});
+			});
+			arrOfKeys.forEach((prop) => {
+				updateNodes("opts", "option", prop);
+			});
+		});
 	});
 })();
