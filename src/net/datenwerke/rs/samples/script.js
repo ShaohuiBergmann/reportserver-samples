3e87cd815da7eb7ad814039491dc97834169b882
ShaohuiBergmann <milashaohuibergmann@gmail.com>
2023-01-25 15:15:47 +0100

    set the values of latest log file as default value of corresponding Input elements

--- src/net/datenwerke/rs/samples/script.js
+++ src/net/datenwerke/rs/samples/script.js
@@ -22,16 +22,16 @@
 	const getFilteredFileArr = (arr) => {
 		const decoded = JSON.parse(atob(arr));
 		const fileArr = decoded["data"];
+
 		return filterFileArr(fileArr);
 	};
 	///////////create new array of objects that has the filepath as key and content as value
-	const createNewArrOfVar = (arr) => {
+	const createNewArrOfObj = (arr) => {
 		const newArrOfVal = [];
 		arr.forEach((el) => {
 			const arrOfVal = Object.values(el);
 			const newObj = createObj(arrOfVal);
 			newArrOfVal.push(newObj);
-			// console.log(newArrOfVal);
 		});
 
 		return newArrOfVal;
@@ -57,11 +57,27 @@
 
 		return filteredFileArr;
 	};
+	const getLastItemInArr = (arr) => {
+		return arr[arr.length - 1];
+	};
 
+	const getDateWithMatcher = (obj) => {
+		const regex = /\d{4}.\d{2}.\d{2}/gm;
+		return Object.keys(obj)[0].match(regex)[0];
+	};
+
+	const filterFilesWithDate = (arr, dateVal) => {
+		return arr.filter((filtered) => {
+			Object.keys(filtered)[0].includes(dateVal);
+		});
+	};
 	///////////functions to manipulate option elements to Dom
 
 	const updateNodes = (selector, element, text) => {
 		const ele = document.createElement(element);
+		if (element === "textarea") {
+			ele.setAttribute("readonly", true);
+		}
 		const node = document.createTextNode(text);
 		ele.appendChild(node);
 		document.getElementById(selector).appendChild(ele);
@@ -80,45 +96,66 @@
 		ele.appendChild(node);
 		document.getElementById(selector).appendChild(ele);
 	};
+	/////// function to set current Date as default value of date Input
+	Date.prototype.toDateInputValue = function (latestDate) {
+		var local = new Date(latestDate);
+		local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+		return local.toJSON().slice(0, 10);
+	};
 
 	//////////////////event listener
-	$("#dateVal").change((e) => {
+	window.addEventListener("load", () => {
+		console.log("page is fully loaded");
+
+		const defaultDateVal = $("#dateVal").val();
+		// console.log(defaultDateVal);
 		getContent()
 			.then((result) => {
-				// clear the content of options and log content at everytime choose another day
-				removeNodes("opts");
-				removeNodes("logContent");
 				const filteredFileArr = getFilteredFileArr(result);
-				const newArrOfVal = createNewArrOfVar(filteredFileArr);
-				const dateInputVal = e.target.value;
+				const newArrOfObj = createNewArrOfObj(filteredFileArr);
+				const lastItemInArr = getLastItemInArr(newArrOfObj);
+				const latestDateOfLogs = getDateWithMatcher(lastItemInArr);
 
-				//filter out all the obj that don't match the date
-
-				const filteredArr = newArrOfVal.filter((filtered) => {
-					return Object.keys(filtered)[0].includes(dateInputVal);
-				});
-
-				if (filteredArr.length == 0) {
-					handleEmptyResult("opts", "option");
-				} else {
+				$("#dateVal").val(new Date().toDateInputValue(latestDateOfLogs));
+				updateNodes("opts", "option", Object.keys(lastItemInArr)[0]);
+				updateNodes(
+					"logContent",
+					"textarea",
+					lastItemInArr[Object.keys(lastItemInArr)[0]]
+				);
+				filterFilesWithDate(newArrOfObj, defaultDateVal);
+				$("#dateVal").change((e) => {
 					removeNodes("opts");
-					filteredArr.forEach((el) => {
-						updateNodes("opts", "option", Object.keys(el));
-					});
-				}
-				$("#opts").change((e) => {
 					removeNodes("logContent");
-					const valOfOpts = e.target.value;
-					const selectedFile = filteredArr.filter((el) => {
-						return el[valOfOpts];
-					});
-					// console.log("select", valOfOpts);
-					// console.log("select", selectedFile);
-					if (selectedFile.length == 0) {
-						handleEmptyResult("logContent", "p");
+					// const filteredFileArr = getFilteredFileArr(result);
+					// const newArrOfVal = createNewArrOfVar(filteredFileArr);
+					const dateInputVal = e.target.value;
+
+					//filter out all the obj that don't match the date
+					const filteredArr = filterFilesWithDate(newArrOfVal, dateInputVal);
+
+					if (filteredArr.length == 0) {
+						handleEmptyResult("opts", "option");
 					} else {
-						updateNodes("logContent", "p", selectedFile[0][valOfOpts]);
+						removeNodes("opts");
+						filteredArr.forEach((el) => {
+							updateNodes("opts", "option", Object.keys(el));
+						});
 					}
+					$("#opts").change((e) => {
+						removeNodes("logContent");
+						const valOfOpts = e.target.value;
+						const selectedFile = filteredArr.filter((el) => {
+							return el[valOfOpts];
+						});
+						// console.log("select", valOfOpts);
+						// console.log("select", selectedFile);
+						if (selectedFile.length == 0) {
+							handleEmptyResult("logContent", "p");
+						} else {
+							updateNodes("logContent", "textarea", selectedFile[0][valOfOpts]);
+						}
+					});
 				});
 			})
 			.catch((e) => console.log("Error", e));
